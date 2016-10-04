16465e31bbec8db2c1a9d91b3e593a96e01d5270
tonnique <tonique@bk.ru>
2016-10-04 15:23:27 +0300

    TypeTest is a C# WinForms App

---
+++ TypeTest/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace TypeTest
+{
+    partial class frmTyper
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.rchTxtBoxTask = new System.Windows.Forms.RichTextBox();
+            this.rchTxtBoxUser = new System.Windows.Forms.RichTextBox();
+            this.lblLang = new System.Windows.Forms.Label();
+            this.cmbTask = new System.Windows.Forms.ComboBox();
+            this.numUpDownMinutes = new System.Windows.Forms.NumericUpDown();
+            this.lblErrorsText = new System.Windows.Forms.Label();
+            this.lblTimeText = new System.Windows.Forms.Label();
+            this.lblMins = new System.Windows.Forms.Label();
+            this.lblErrors = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(2, 37);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.rchTxtBoxTask);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rchTxtBoxUser);
+            this.splitContainer1.Size = new System.Drawing.Size(790, 365);
+            this.splitContainer1.SplitterDistance = 208;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // rchTxtBoxTask
+            // 
+            this.rchTxtBoxTask.BackColor = System.Drawing.Color.White;
+            this.rchTxtBoxTask.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rchTxtBoxTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rchTxtBoxTask.ForeColor = System.Drawing.Color.Black;
+            this.rchTxtBoxTask.Location = new System.Drawing.Point(0, 0);
+            this.rchTxtBoxTask.Name = "rchTxtBoxTask";
+            this.rchTxtBoxTask.ReadOnly = true;
+            this.rchTxtBoxTask.Size = new System.Drawing.Size(790, 208);
+            this.rchTxtBoxTask.TabIndex = 4;
+            this.rchTxtBoxTask.Text = "";
+            // 
+            // rchTxtBoxUser
+            // 
+            this.rchTxtBoxUser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rchTxtBoxUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rchTxtBoxUser.Location = new System.Drawing.Point(0, 0);
+            this.rchTxtBoxUser.Name = "rchTxtBoxUser";
+            this.rchTxtBoxUser.Size = new System.Drawing.Size(790, 153);
+            this.rchTxtBoxUser.TabIndex = 1;
+            this.rchTxtBoxUser.Text = "";
+            this.rchTxtBoxUser.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rchTxtBoxUser_KeyPress);
+            // 
+            // lblLang
+            // 
+            this.lblLang.AutoSize = true;
+            this.lblLang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLang.Location = new System.Drawing.Point(2, 7);
+            this.lblLang.Name = "lblLang";
+            this.lblLang.Size = new System.Drawing.Size(27, 16);
+            this.lblLang.TabIndex = 6;
+            this.lblLang.Text = "EN";
+            this.lblLang.Click += new System.EventHandler(this.lblLang_Click);
+            // 
+            // cmbTask
+            // 
+            this.cmbTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmbTask.FormattingEnabled = true;
+            this.cmbTask.Location = new System.Drawing.Point(33, 4);
+            this.cmbTask.Name = "cmbTask";
+            this.cmbTask.Size = new System.Drawing.Size(170, 24);
+            this.cmbTask.TabIndex = 2;
+            this.cmbTask.SelectedIndexChanged += new System.EventHandler(this.cmbTask_SelectedIndexChanged);
+            // 
+            // numUpDownMinutes
+            // 
+            this.numUpDownMinutes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numUpDownMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numUpDownMinutes.Location = new System.Drawing.Point(466, 7);
+            this.numUpDownMinutes.Name = "numUpDownMinutes";
+            this.numUpDownMinutes.Size = new System.Drawing.Size(77, 22);
+            this.numUpDownMinutes.TabIndex = 3;
+            this.numUpDownMinutes.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // lblErrorsText
+            // 
+            this.lblErrorsText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblErrorsText.AutoSize = true;
+            this.lblErrorsText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblErrorsText.Location = new System.Drawing.Point(571, 11);
+            this.lblErrorsText.Name = "lblErrorsText";
+            this.lblErrorsText.Size = new System.Drawing.Size(47, 16);
+            this.lblErrorsText.TabIndex = 9;
+            this.lblErrorsText.Text = "Errors:";
+            // 
+            // lblTimeText
+            // 
+            this.lblTimeText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTimeText.AutoSize = true;
+            this.lblTimeText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTimeText.Location = new System.Drawing.Point(660, 12);
+            this.lblTimeText.Name = "lblTimeText";
+            this.lblTimeText.Size = new System.Drawing.Size(45, 16);
+            this.lblTimeText.TabIndex = 10;
+            this.lblTimeText.Text = "Time: ";
+            // 
+            // lblMins
+            // 
+            this.lblMins.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMins.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMins.Location = new System.Drawing.Point(406, 11);
+            this.lblMins.Name = "lblMins";
+            this.lblMins.Size = new System.Drawing.Size(54, 17);
+            this.lblMins.TabIndex = 11;
+            this.lblMins.Text = "Minutes";
+            // 
+            // lblErrors
+            // 
+            this.lblErrors.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblErrors.AutoSize = true;
+            this.lblErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblErrors.Location = new System.Drawing.Point(614, 12);
+            this.lblErrors.Name = "lblErrors";
+            this.lblErrors.Size = new System.Drawing.Size(29, 16);
+            this.lblErrors.TabIndex = 12;
+            this.lblErrors.Text = "000";
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTime.Location = new System.Drawing.Point(699, 12);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(15, 16);
+            this.lblTime.TabIndex = 13;
+            this.lblTime.Text = "0";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.Location = new System.Drawing.Point(720, 7);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(60, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmTyper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 405);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblErrors);
+            this.Controls.Add(this.lblMins);
+            this.Controls.Add(this.lblTimeText);
+            this.Controls.Add(this.lblErrorsText);
+            this.Controls.Add(this.numUpDownMinutes);
+            this.Controls.Add(this.cmbTask);
+            this.Controls.Add(this.lblLang);
+            this.Controls.Add(this.splitContainer1);
+            this.MinimumSize = new System.Drawing.Size(810, 444);
+            this.Name = "frmTyper";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TypingTest";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmTyper_FormClosed);
+            this.Shown += new System.EventHandler(this.frmTyper_Shown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox rchTxtBoxTask;
+        private System.Windows.Forms.RichTextBox rchTxtBoxUser;
+        private System.Windows.Forms.Label lblLang;
+        private System.Windows.Forms.ComboBox cmbTask;
+        private System.Windows.Forms.NumericUpDown numUpDownMinutes;
+        private System.Windows.Forms.Label lblErrorsText;
+        private System.Windows.Forms.Label lblTimeText;
+        private System.Windows.Forms.Label lblMins;
+        private System.Windows.Forms.Label lblErrors;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ TypeTest/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO; 
+
+namespace TypeTest
+{
+    public partial class frmTyper : Form
+    {
+        private const int numOfTextPatternsForTask = 200;
+        private const string filesPath = @"..\..\Files\";
+        private const string messageRus = "Начните печатать здесь и время начнёт отсчёт.";
+        private const string messageEn = "Start typing here and time will begin count up.";
+
+        private bool startTyping = false;
+        private int mistypes = 0;
+        private int textPosition = 0;        
+        private string taskString = string.Empty;
+        private int secondsWent = 0; //seconds spent
+        private int spm = 0; //SymbolPerMinutes
+        private Thread timeThread;
+
+        #region menu arrays
+        private string[] russTasks = new string[] {
+            "Задание 1 (ао)",
+            "Задание 2 (а о)" ,
+            "Выражения"
+        };
+
+        private string[] engTasks = new string[] {
+            "Task 1 (dk)",
+            "Task 2 (d k)",
+            "Aesop Fables",
+            "Space Cowboys",
+            "TigerInTheWild",
+            "Phrases"
+        };
+        #endregion 
+
+        string[] rusTaskFile = new string[] {"", "", "phrases_censored.ru"};
+        string[] engTaskFile = new string[] {"", "", "Aesop", "SpaceCowboys",  "TigerInTheWild", "phrases.en"};
+
+        public frmTyper()
+        {
+            InitializeComponent();
+        }
+
+        // Changing the Language of the typing test 
+        private void lblLang_Click(object sender, EventArgs e)
+        {
+            cmbTask.Items.Clear();
+            if (lblLang.Text == "RU")
+            { 
+                lblLang.Text = "EN";                
+                cmbTask.Items.AddRange(engTasks);                
+            }
+            else 
+            {
+                lblLang.Text = "RU";                
+                cmbTask.Items.AddRange(russTasks);                
+            }
+            
+            cmbTask.SelectedIndex = 0;
+            cmbTask.SelectionLength = cmbTask.Text.Length;            
+        }
+
+        private void cmbTask_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cmb = sender as ComboBox;
+            rchTxtBoxTask.Clear();
+
+            if (lblLang.Text == "RU")
+            {
+                switch (cmb.SelectedIndex)
+                { 
+                    case 0:
+                        
+                        taskString = prepareTask("ао");                        
+                        break;
+                    case 1:            
+                        taskString = prepareTask("а о");                        
+                        break;
+                    case 2:
+                        taskString = readFile(rusTaskFile[cmb.SelectedIndex]);
+                        break;
+                }                
+            }
+            else
+            {
+                switch (cmb.SelectedIndex)
+                {
+                    case 0:                        
+                        taskString = prepareTask("dk");                        
+                        break;
+                    case 1:                        
+                        taskString = prepareTask("d k");
+                        break;
+                    case 2:                    
+                    case 3:                        
+                    case 4:                    
+                    case 5:
+                        taskString = readFile(engTaskFile[cmb.SelectedIndex]);
+                        break;
+                }
+            
+            }
+            // Text for an exercise
+            rchTxtBoxTask.Text = taskString;
+
+            textPosition = 0; // Set to zero position
+            mistypes = 0; // Set to zero errors
+            lblErrors.Text = mistypes.ToString(); // Change information about errors 
+
+            secondsWent = 0; // time went
+            lblTime.Text = secondsWent.ToString(); 
+
+            putCommentIntoUserControl(); // a marker if we start typing
+            startTyping = false; // Set comment, where to type and when timer is active            
+        }
+
+        // Fills a resulting text for task with given pattern
+        private string prepareTask(string s)
+        {
+            string tempStr = string.Empty;
+
+            for (int i = 0; i < numOfTextPatternsForTask; i++) 
+            {
+                tempStr += s + " ";
+            }
+            
+            return tempStr;
+        }
+
+        //This method handles event which occurs when Form is Shown for the first time
+        private void frmTyper_Shown(object sender, EventArgs e)
+        {
+            lblErrors.Text = mistypes.ToString();
+            lblLang.Text = "EN";
+            cmbTask.Items.AddRange(engTasks);
+            cmbTask.SelectedIndex = 0;            
+            putCommentIntoUserControl();
+        }
+
+        private void rchTxtBoxUser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!startTyping)
+            {
+                rchTxtBoxTask.Select(0, taskString.Length);
+                rchTxtBoxTask.SelectionColor = System.Drawing.Color.Black;
+                rchTxtBoxTask.Select(0, 0);
+
+                rchTxtBoxUser.Clear();
+                startTyping = true;
+
+                timeThread = new Thread(timeCountDown);
+                timeThread.Name = "timeThread";
+                timeThread.Start();
+            }
+
+            if (e.KeyChar.ToString() == taskString.Substring(textPosition, 1))
+            {
+                
+                rchTxtBoxTask.Select(textPosition, 1);
+                rchTxtBoxTask.SelectionColor = System.Drawing.Color.Blue;
+                rchTxtBoxTask.ScrollToCaret();
+
+                rchTxtBoxUser.Select(textPosition, 1);
+                rchTxtBoxUser.SelectionColor = System.Drawing.Color.Black;
+                textPosition++;
+
+                if (textPosition >= taskString.Length)
+                { 
+                    typeTestFinish();
+                }
+            }
+            else
+            {
+                if (e.KeyChar.ToString() != "\b")
+                {
+                    mistypes++;
+                    lblErrors.Text = mistypes.ToString();
+                    rchTxtBoxUser.Select(textPosition, 1);
+                    rchTxtBoxUser.SelectionColor = System.Drawing.Color.Red;                    
+                }
+            }
+        }
+
+        //This method puts the comment into rchTxtBoxUser telling a user where to type and when timer is activated
+        private void putCommentIntoUserControl()
+        {
+            string tmpMessage = (lblLang.Text == "RU") ? messageRus : messageEn;
+
+            if (rchTxtBoxUser.InvokeRequired) 
+            {
+                rchTxtBoxUser.Invoke(new Action<string>((s) => rchTxtBoxUser.Text = s), tmpMessage);
+                rchTxtBoxUser.Invoke(new Action(() => rchTxtBoxUser.Select(0, rchTxtBoxUser.Text.Length)));
+                rchTxtBoxUser.Invoke(new Action(() => rchTxtBoxUser.SelectionColor = System.Drawing.Color.Gray));                
+                rchTxtBoxUser.Invoke(new Action(() => rchTxtBoxUser.Select(0, 0)));
+                rchTxtBoxUser.Invoke(new Action(() => rchTxtBoxUser.Focus()));
+            }
+            else {
+                rchTxtBoxUser.Text = tmpMessage;
+                rchTxtBoxUser.Select(0, rchTxtBoxUser.Text.Length);
+                rchTxtBoxUser.SelectionColor = System.Drawing.Color.Gray;
+                rchTxtBoxUser.Select(0, 0);
+                rchTxtBoxUser.Focus();
+            }            
+        }
+
+        private void timeCountDown()
+        {
+            int secondsLeft = int.Parse(numUpDownMinutes.Value.ToString()) * 60;            
+
+            while (secondsLeft > 0 && startTyping == true)
+            {
+                lblTime.Invoke(new Action<string>((s) => lblTime.Text = s), secondsWent.ToString()); 
+                Thread.Sleep(1000);
+                secondsLeft--;
+                secondsWent++;
+                
+                spm = (int)((float)textPosition / ((float)secondsWent / 60));
+            }
+
+            if (startTyping) 
+                typeTestFinish();
+        }
+
+        private void ShowEndStatistics()
+        { 
+            string message = string.Empty;
+            if(lblLang.Text == "RU")
+            {
+                message = "Время вышло!\n";
+                message += "Потрачено времени: ";
+                if (secondsWent > 60)
+                {
+                    message += secondsWent / 60 + "мин. ";
+                    secondsWent = secondsWent % 60;                    
+                }
+                
+                message += (secondsWent > 0) ? secondsWent + " сек.\n" : "\n";
+
+                message += "Скорость набора: " + spm +" символов в минуту\n";
+                message += "Процент ошибок: " + (float)(mistypes * 100 / textPosition) + " %";
+            }
+            else
+            {
+                message = "Time's up!\n";
+                message += "Spent time: ";
+                if (secondsWent > 60)
+                {
+                    message += secondsWent / 60 + "min. ";
+                    secondsWent = secondsWent % 60;
+                }
+
+                message += (secondsWent > 0) ? secondsWent + " sec.\n" : "\n";
+
+                message += "Type speed: " + spm + " symbols per minute\n";
+                message += "Errors : " + (float)(mistypes * 100 / textPosition) + " %";               
+            }           
+
+            MessageBox.Show(message);
+        }
+
+        private void frmTyper_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (timeThread != null) { timeThread.Abort(); }            
+        }
+
+        private void typeTestFinish()
+        {
+            if (startTyping)
+            {
+                startTyping = false;
+                ShowEndStatistics();
+            }
+
+            mistypes = 0;
+                        
+            
+            if (lblErrors.InvokeRequired) { lblErrors.Invoke(new Action<string>((s) => lblErrors.Text = s), "0"); }
+            else { lblErrors.Text = "0"; }
+            if (lblTime.InvokeRequired) { lblTime.Invoke(new Action<string>((s) => lblTime.Text = s), "0"); }
+            else { lblTime.Text = "0";}            
+        
+            textPosition = 0;
+            secondsWent = 0;
+
+            putCommentIntoUserControl();           
+
+            if (timeThread != null)
+            {
+                try
+                {
+                    timeThread.Abort();
+                }
+                catch { }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {            
+            typeTestFinish();            
+        }
+
+        private string readFile(string file)
+        { 
+            string strTemp = string.Empty;
+
+            if (File.Exists(filesPath + file))
+            {
+                try
+                {
+                    strTemp = File.ReadAllText(filesPath + file);
+                }
+                catch { MessageBox.Show("Error while opening file " + file);}
+            }
+            return strTemp;
+        }
+    
+    }
+}
